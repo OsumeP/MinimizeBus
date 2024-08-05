d38a6a7a091ae283a7bc0086bc0ab2e6e2be9018
OsumeP <juandavid.patarroyosanchez@gmail.com>
2024-08-05 12:35:56 -0500

    Minor change to improve execution time and documentation

--- src/main/java/com/mycompany/rutasautobus/Model/GraphManager.java
+++ src/main/java/com/mycompany/rutasautobus/Model/GraphManager.java
@@ -23,8 +23,11 @@ public class GraphManager {
     
     // Metodo encargado de crear el gráfo apenas se selecciona el archivo.
     public GraphManager(File file){
+        // HashMap diseñado para asignar un indice a cada parada
         this.indexes = new HashMap<String, Integer>();
+        //Grafo representado por medio de una matriz de listas
         this.graph = new ArrayList<>();
+        //Contador que representa el número de vértices.
         this.vertexCount = 0;
         try (FileReader fr = new FileReader(file)) {
             BufferedReader br = new BufferedReader(fr);
@@ -36,6 +39,7 @@ public class GraphManager {
                 String[] arr = line.split(" ");
                 count--;
                 for(int i = 1; i < arr.length - 2; i += 2){
+                    //Si la parada no está indexada, lo hace. Aumenta en 1 la cantidad de vertices
                     if(!indexes.containsKey(arr[i])){
                         indexes.putIfAbsent(arr[i], vertexCount++);
                         graph.add(new ArrayList<Pair>());
@@ -45,6 +49,7 @@ public class GraphManager {
                     Pair<String, Integer> curr = new Pair<String, Integer>(arr[i+2], Integer.valueOf(arr[i+1]));
                     graph.get(indexes.get(arr[i])).add(curr);
                 }
+                //Se indexa la última parada de la ruta si no lo estaba.
                 if(!indexes.containsKey(arr[arr.length - 1])){
                     indexes.putIfAbsent(arr[arr.length - 1], vertexCount++);
                     graph.add(new ArrayList<Pair>());
@@ -54,7 +59,6 @@ public class GraphManager {
         catch (Exception e) {
             System.out.println(e);
         }
-        System.out.println(graph);
     }
     
     // Este método es una implementación del algoritmo de Dijsktra que se ejecuta cuando el usuario
@@ -64,11 +68,14 @@ public class GraphManager {
         Comparator<Pair> comparator = (Pair o1, Pair o2) -> o1.getSecond().compareTo(o2.getSecond());
         //Creación de la cola de prioridad.
         PriorityQueue<Pair> queue = new PriorityQueue<Pair>(comparator);
-        Trio[] track = new Trio[vertexCount];
-
-        //Creación de un hashMap "Track" que llevará el registro del camino más corto a las paradas por medio de Trios.
+        //Arreglo de "Trio" para trackear los caminos más cortos a cada vértice.
         //Trio: vertice predecesor, costo, número de vertices.
-        if(indexes.get(origin) == null){
+        Trio[] track = new Trio[vertexCount];
+        
+        boolean[] completed = new boolean[vertexCount];
+        
+        //Si el nombre del origen o meta no esta indexado, significa que no está en el gráfo, por lo tanto no hay camino.
+        if(indexes.get(origin) == null || indexes.get(goal) == null){
             return null;
         }
         track[indexes.get(origin)] = new Trio<String, Integer, Integer>(null, 0, 0);
@@ -83,6 +90,10 @@ public class GraphManager {
             int index = this.indexes.get(name);
             Trio data = track[index];
             
+            if(completed[index]){
+                continue;
+            }
+            //Si el nombre del nodo es igual a la meta, entonces ya encontramos el camino más corto.
             if(name.compareTo(goal) == 0){
                 return RebuildPath(origin, goal, track);
             }
@@ -93,23 +104,29 @@ public class GraphManager {
                 if(indexes.get(nameChild) == null) continue;
                 int indexChild = indexes.get(nameChild);
                 
+                //Si vertice no ha sido descubierto, le añade el nuevo trio al arreglo.
                 if(track[indexChild] == null){
                     track[indexChild] = new Trio(name, cost + costChild,(Integer) data.getThird() + 1);
                     queue.add(new Pair<>(nameChild, cost + costChild));
                 }
+                //Si el costo de esta nueva ruta es menor al que estaba registrado, lo remplazamos.
                 else if((Integer) track[indexChild].getSecond() > costChild + cost){
                     track[indexChild] = new Trio(name, cost + costChild, (Integer) data.getThird() + 1);
                     queue.add(new Pair<>(nameChild, cost + costChild));
                 }
+                //Si el costo es igual al registrado, revisamos la cantidad de nodos necesarios para saber
+                //si necesita menos paradas.
                 else if((Integer) track[indexChild].getSecond() == costChild + cost && (Integer) track[indexChild].getThird() > (Integer) data.getThird() + 1){
                     track[indexChild] = new Trio(name, cost + costChild, (Integer) data.getThird() + 1);
                 }
             }
+            completed[index] = true;
         }
         
         return null;
     }
     
+    //Método dedicado a reconstruir el camino desde la meta hasta el origen.
     private List<String> RebuildPath(String origin, String goal, Trio[] track){
         String name = goal;
         Trio<String, Integer, Integer> data;
