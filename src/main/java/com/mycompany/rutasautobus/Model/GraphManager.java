81191c24f9077705f35c6e852d13d850ddc1f208
OsumeP <juandavid.patarroyosanchez@gmail.com>
2024-08-05 08:56:36 -0500

    Opmtimized solution

--- src/main/java/com/mycompany/rutasautobus/Model/GraphManager.java
+++ src/main/java/com/mycompany/rutasautobus/Model/GraphManager.java
@@ -17,11 +17,15 @@ import java.util.Comparator;
  * @author Juand
  */
 public class GraphManager {
-    HashMap<String, List<Pair>> graph;
+    HashMap<String, Integer> indexes;
+    List<List<Pair>> graph;
+    int vertexCount;
     
     // Metodo encargado de crear el gráfo apenas se selecciona el archivo.
     public GraphManager(File file){
-        this.graph = new HashMap<String, List<Pair>>();
+        this.indexes = new HashMap<String, Integer>();
+        this.graph = new ArrayList<>();
+        this.vertexCount = 0;
         try (FileReader fr = new FileReader(file)) {
             BufferedReader br = new BufferedReader(fr);
             String line;
@@ -30,53 +34,75 @@ public class GraphManager {
             while(count > 0 && (line = br.readLine())!= null){
                 // Lista de paradas en la ruta
                 String[] arr = line.split(" ");
-                System.out.println(Arrays.toString(arr));
                 count--;
                 for(int i = 1; i < arr.length - 2; i += 2){
-                    graph.putIfAbsent(arr[i], new ArrayList<Pair>());
+                    if(!indexes.containsKey(arr[i])){
+                        indexes.putIfAbsent(arr[i], vertexCount++);
+                        graph.add(new ArrayList<Pair>());
+                    }
+                    
+                    // Par de datos (nombre de la parada, costo) se añade a la lista correspondiente;
                     Pair<String, Integer> curr = new Pair<String, Integer>(arr[i+2], Integer.valueOf(arr[i+1]));
-                    graph.get(arr[i]).add(curr);
-                    System.out.println("Key: " + arr[i] + " " + graph.get(arr[i]));
+                    graph.get(indexes.get(arr[i])).add(curr);
+                }
+                if(!indexes.containsKey(arr[arr.length - 1])){
+                    indexes.putIfAbsent(arr[arr.length - 1], vertexCount++);
+                    graph.add(new ArrayList<Pair>());
                 }
             }
         }
         catch (Exception e) {
             System.out.println(e);
         }
+        System.out.println(graph);
     }
     
+    // Este método es una implementación del algoritmo de Dijsktra que se ejecuta cuando el usuario
+    //define la parada de origen y destino.
     public List<String> Dijsktra(String origin, String goal){
+        //Definicion de objeto comparador para que compare la propiedad "second" del objeto Pair.
         Comparator<Pair> comparator = (Pair o1, Pair o2) -> o1.getSecond().compareTo(o2.getSecond());
+        //Creación de la cola de prioridad.
         PriorityQueue<Pair> queue = new PriorityQueue<Pair>(comparator);
-        //Trio: Previous vertex name, cost, number of vertexs.
-        HashMap<String, Trio> track = new HashMap<String, Trio>();
-        track.put(origin, new Trio<String, Integer, Integer>(null, 0, 0));
+        Trio[] track = new Trio[vertexCount];
+
+        //Creación de un hashMap "Track" que llevará el registro del camino más corto a las paradas por medio de Trios.
+        //Trio: vertice predecesor, costo, número de vertices.
+        if(indexes.get(origin) == null){
+            return null;
+        }
+        track[indexes.get(origin)] = new Trio<String, Integer, Integer>(null, 0, 0);
         queue.add(new Pair<String, Integer>(origin, 0));
         
+        //Ciclo while que se ejecuta mientras la cola de prioridad tenga elementos o hasta que se
+        //ejecute el algoritmo sobre el nodo destino, significando que se encontró el camino más corto.
         while(!queue.isEmpty()){
             Pair node = queue.poll();
             int cost = (Integer) node.getSecond();
             String name = (String) node.getFirst();
-            Trio data = track.get(name);
+            int index = this.indexes.get(name);
+            Trio data = track[index];
             
             if(name.compareTo(goal) == 0){
                 return RebuildPath(origin, goal, track);
             }
-            if(graph.get(name) == null) continue;
-            for(Pair i : graph.get(name)){
+            if(graph.get(index) == null) continue;
+            for(Pair i : graph.get(index)){
                 String nameChild = (String) i.getFirst();
                 int costChild = (Integer) i.getSecond();
+                if(indexes.get(nameChild) == null) continue;
+                int indexChild = indexes.get(nameChild);
                 
-                if(!track.containsKey( nameChild ) ){
-                    track.put((String) i.getFirst(), new Trio(name, cost + costChild,(Integer) data.getThird() + 1));
+                if(track[indexChild] == null){
+                    track[indexChild] = new Trio(name, cost + costChild,(Integer) data.getThird() + 1);
                     queue.add(new Pair<>(nameChild, cost + costChild));
                 }
-                else if((Integer) track.get(nameChild).getSecond() > costChild + cost){
-                    track.replace(nameChild, new Trio(name, cost + costChild, (Integer) data.getThird() + 1));
+                else if((Integer) track[indexChild].getSecond() > costChild + cost){
+                    track[indexChild] = new Trio(name, cost + costChild, (Integer) data.getThird() + 1);
                     queue.add(new Pair<>(nameChild, cost + costChild));
                 }
-                else if((Integer) track.get(nameChild).getSecond() == costChild + cost && (Integer) track.get(nameChild).getThird() > (Integer) data.getThird() + 1){
-                    track.replace(nameChild, new Trio(name, cost + costChild, (Integer) data.getThird() + 1));
+                else if((Integer) track[indexChild].getSecond() == costChild + cost && (Integer) track[indexChild].getThird() > (Integer) data.getThird() + 1){
+                    track[indexChild] = new Trio(name, cost + costChild, (Integer) data.getThird() + 1);
                 }
             }
         }
@@ -84,12 +110,12 @@ public class GraphManager {
         return null;
     }
     
-    private List<String> RebuildPath(String origin, String goal, HashMap<String, Trio> track){
+    private List<String> RebuildPath(String origin, String goal, Trio[] track){
         String name = goal;
         Trio<String, Integer, Integer> data;
-        List<String> result = new ArrayList<>((Integer) track.get(goal).getThird());
+        List<String> result = new ArrayList<>((Integer) track[indexes.get(goal)].getThird());
         while(name != null){
-            data = track.get(name);
+            data = track[indexes.get(name)];
             result.add(name);
             name = data.getFirst();
         }
